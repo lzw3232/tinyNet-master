d6de80c1dbd9ffc1eb9648cfe4b9bf427cdb94d4
lzw3232 <lzw32321226@163.com>
2020-10-11 00:21:32 +0800

    1

--- src/app/routes/device/electric-boiler-detail/view/view.component.ts
+++ src/app/routes/device/electric-boiler-detail/view/view.component.ts
@@ -11,7 +11,16 @@ const DataSet = require('@antv/data-set');
   templateUrl: './view.component.html',
 })
 export class ElectricBoilerDetailViewComponent implements OnInit {
+  record: any = {};
+  i: any;
 
+  data;
+  forceFit = true;
+  width = 400;
+  height = 400;
+  style = { stroke: '#fff', lineWidth: 1 };
+  chart_title_x = {text: '个数', textStyle: {fill: '#515151'} };
+  chart_title_y = {text: '成本(元)', textStyle: {fill: '#515151'}};
   constructor(
     private modal: NzModalRef,
     public msgSrv: NzMessageService,
@@ -20,7 +29,39 @@ export class ElectricBoilerDetailViewComponent implements OnInit {
   ) { }
 
   ngOnInit(): void {
+    console.log(this.record);
+    console.log(this.i);
+    this.devicesService.select(this.record.id,"electric_boiler").subscribe((res)=>{
+      console.log(res);
+      if(res["errno"]=="0"){
+        this.i = res["data"]["data"]["data"];
+        const sourceData: any[] = [
+          {x : this.i.capacity1, 初建成本 : this.i.initialCost1, 替换成本 : this.i.replacementCost1, 运维成本 : this.i.maintenanceCost1},
+          {x : this.i.capacity2, 初建成本 : this.i.initialCost2, 替换成本 : this.i.replacementCost2, 运维成本 : this.i.maintenanceCost2},
+          {x : this.i.capacity3, 初建成本 : this.i.initialCost3, 替换成本 : this.i.replacementCost3, 运维成本 : this.i.maintenanceCost3},
+          {x : this.i.capacity4, 初建成本 : this.i.initialCost4, 替换成本 : this.i.replacementCost4, 运维成本 : this.i.maintenanceCost4},
+        ];
 
+        const dv = new DataSet.View().source(sourceData);
+        dv.transform({
+          type: 'fold',
+          fields: ['初建成本', '替换成本', '运维成本'],
+          key: 'cost_type',
+          value: 'cost_number',
+        });
+        const data = dv.rows;
+        this.data = data;
+      }
+      else if(res["errno"]=="2"){
+        this.devicesService.tologin();
+      }
+      else{
+        this.msgSrv.create('error', `error`);
+      }
+      this.devicesService.setCookie("token",res["data"]["data"]["token"]);
+    })
   }
-
+  close() {
+    this.modal.destroy();
+  }
 }
--- src/app/routes/device/host-detail/host-detail.component.ts
+++ src/app/routes/device/host-detail/host-detail.component.ts
@@ -1,3 +1,33 @@
+// import {Component, OnInit, Output, ViewChild} from '@angular/core';
+// import {_HttpClient, MenuService, ModalHelper} from '@delon/theme';
+// import {STChange, STColumn, STComponent, STPage} from '@delon/abc';
+// import { SFSchema } from '@delon/form';
+// import {HostDetailViewComponent} from './view/view.component';
+// import {HostDetailEditComponent} from './edit/edit.component'
+// import {DevicesService} from "../../../user-service/devicesService";
+// import {NzMessageService, NzModalService} from "ng-zorro-antd";
+
+// @Component({
+//   selector: 'app-device-host-detail',
+//   templateUrl: './host-detail.component.html',
+// })
+// export class HostDetailComponent implements OnInit {
+
+
+//   constructor(private http: _HttpClient,
+//               private modal: ModalHelper,
+//               private msgSrv: NzMessageService,
+//               private devicesService: DevicesService,
+//               private modalService: NzModalService,
+//   ) {
+//   }
+
+//   ngOnInit() {
+//   }
+
+
+
+// }
 import {Component, OnInit, Output, ViewChild} from '@angular/core';
 import {_HttpClient, MenuService, ModalHelper} from '@delon/theme';
 import {STChange, STColumn, STComponent, STPage} from '@delon/abc';
@@ -12,7 +42,80 @@ import {NzMessageService, NzModalService} from "ng-zorro-antd";
   templateUrl: './host-detail.component.html',
 })
 export class HostDetailComponent implements OnInit {
+  data : any;
+  pi : Number;
+  ps : Number;
+  total=0;
+  val="";
 
+  searchSchema: SFSchema = {
+    properties: {
+      name: {
+        type: 'string',
+        title: '型号名称'
+      }
+    },
+  };
+  pages: STPage = {
+    total: '',//分页显示多少条数据，字符串型
+    show: true,//显示分页
+    front: false //关闭前端分页，true是前端分页，false后端控制分页
+  };
+  @ViewChild('st') st: STComponent;
+  columns: STColumn[] = [
+    { title: '型号名称', index: 'name' , fixed: 'left', width: '100px'},
+    { title: '制冰能效比(%)', type: 'number', index: 'iceRatio' },
+    { title: '制冷能效比(%)', type: 'number', index: 'refrigerationRatio' },
+    { title: '寿命(年)', type: 'number', index: 'life' },
+    { title: '额定制冰电功率(KW)', type: 'number', index: 'ratedIceElectricPower' },
+    { title: '额定制冷电功率(KW)', type: 'number', index: 'ratedRefrigeratingElectricPower' },
+    { title: '制造商', index: 'manufacturer' },
+    { title: '制冰耗电系数(%)', type: 'number', index: 'iceConsumptionCoefficient' },
+    { title: '制冷耗电系数(%)', type: 'number', index: 'refrigerationConsumptionCoefficient' },
+    {
+      title: '',
+      fixed: 'right', width: '120px',
+      buttons: [
+        { text: '<i class="anticon anticon-file-text"></i>', type: 'static',
+          component: HostDetailViewComponent,
+        },
+        { text: '<i class="anticon anticon-edit"></i>',
+          type: 'static', component: HostDetailEditComponent,
+          click: (record, _modal, comp) => {
+              this.getlist(this.pi);
+          }
+        },
+        { text: '<i class="anticon anticon-delete"></i>',
+          click: (record, _modal, comp) => {
+                this.modalService.confirm({
+                  nzTitle: '确认删除么？？？',
+                  nzOkText: 'OK',
+                  nzCancelText: 'Cancel',
+                  nzOnOk:()=>{
+                    this.devicesService.delete(record.id,"host")
+                      .subscribe((res) => {
+                        console.log(res);
+                        if(res["errno"]=="0"){
+                          this.msgSrv.success(`成功删除 ${record.name}`);
+                        }
+                        else if(res["errno"]=="2"){
+                          this.devicesService.tologin();
+                        }
+                        else{
+                          this.msgSrv.create('error', `error`);
+                        }
+                        this.devicesService.setCookie("token",res["data"]["data"]["token"]);
+                        // comp.removeRow(record);
+                        this.getlist(this.pi);
+                      });
+                  }
+                })
+
+          }
+        },
+      ]
+    }
+  ];
 
   constructor(private http: _HttpClient,
               private modal: ModalHelper,
@@ -23,6 +126,55 @@ export class HostDetailComponent implements OnInit {
   }
 
   ngOnInit() {
+    this.pi = 1;
+    this.ps = 10;
+    this.getlist(this.pi);
+  }
+  add() {
+    this.modal
+      .create(HostDetailEditComponent, { i: { id: 0 } })
+      .subscribe(res=>{
+        if(res){
+          this.getlist(this.pi);
+        }
+      })
+  }
+
+  change(ret: STChange) {
+    if(ret.pi!=this.pi){
+      this.getlist(ret.pi);
+    }
+  }
+
+  getlist(pi){
+    this.devicesService.list(pi,this.ps,this.val,"host").subscribe((res)=>{
+      console.log(res);
+      if(res["errno"]=="0"){
+        this.total = res["data"]["data"]["total"];
+        this.pages.total = '共' + this.total + '条';
+        this.data = res["data"]["data"]["list"];
+        this.pi = pi;
+        // this.ps = 10;
+      }
+      else if(res["errno"]=="2"){
+        this.devicesService.tologin();
+      }
+      else{
+        this.msgSrv.create('error', `error`);
+      }
+      this.devicesService.setCookie("token",res["data"]["data"]["token"]);
+    })
+  }
+
+
+  submit(value:any){
+    this.val = (value["name"]==undefined)?"":value["name"];
+    this.getlist(1);
+  }
+
+  reset(value:any){
+    this.val = "";
+    this.getlist(1);
   }
 
 
